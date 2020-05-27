57df89a3da61bfc89cd0bcec23db09b32dc7baa5
aaron-kane-w <aaron.kane.w@gmail.com>
2020-05-26 20:36:41 -0500

    fetch class assignment complete

--- public/index.js
+++ public/index.js
@@ -1,2 +1,23 @@
-//stuff
-//more stuff
+const url = 'https://my-json-server.typicode.com/jubs16/Products/Products'
+
+
+function getPosts() {
+  fetch(url)
+  .then(response => response.json())
+  .then((posts) => {
+    return posts.forEach(function(x) {
+      const ul = document.getElementById('fetch')
+
+      let img = document.createElement('img')
+      img.src = `${x.imgUrl}`
+
+      let li = document.createElement('li')
+      li.innerHTML = `${x.name} $${x.price}`
+     
+      ul.appendChild(li)
+      ul.appendChild(img)
+    })
+  
+  })
+}
+getPosts()
